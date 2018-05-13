791b400a485b29c7307f263f099ab115c205bed4
Mukarram <mukarram@Mukarrams-MacBook-Pro.local>
2018-05-14 02:09:32 +0530

    Working

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,45 +1,132 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-import Header from '../components/header'
-import './index.css'
+import "./index.css";
+import "../styles/layout-overide.css";
 
-const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
+import Media from "react-media";
+
+const Header = () => (
+  <div
+    style={{
+      background: "#f5f5f5",
+      marginBottom: "3rem",
+      borderBottom: "2px solid #e6e6e6"
+    }}
+  >
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        margin: "0 auto",
+        maxWidth: 980,
+        padding: "1.45rem 1.0875rem"
       }}
     >
-      {children()}
+      <h1 style={{ margin: 0, textAlign: "center", fontSize: "18px" }}>
+        <Link
+          to="/"
+          style={{
+            color: "black",
+            textDecoration: "none"
+          }}
+        >
+          Chai with Mukku
+        </Link>
+      </h1>
     </div>
   </div>
-)
+);
 
-Layout.propTypes = {
-  children: PropTypes.func,
-}
-
-export default Layout
-
-export const query = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
+const Sidebar = props => (
+  <div
+    style={{
+      border: "2px solid #e6e6e6",
+      maxWidth: 960,
+      padding: "0.5rem",
+      marginBottom: "25px"
+    }}
+  >
+    {props.link ?
+      (<Link to={props.link}><strong>{props.title}</strong></Link>) :
+      (<strong>{props.title}<br/></strong>)
     }
-  }
-`
+    {props.description}
+  </div>
+);
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Personal Blog"
+      meta={[
+        { name: "description", content: "Tech" },
+        { name: "keywords", content: "tech, ideas" }
+      ]}
+    />
+    <Header />
+    <div
+      style={{
+        margin: "0 auto",
+        maxWidth: 980,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: "100%"
+      }}
+    >
+      <Media query={{ maxWidth: 848 }}>
+        {matches =>
+          matches ? (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 1 }}>{children()}</div>
+            </div>
+          ) : (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 2.5, paddingRight: "30px" }}>
+                {children()}
+              </div>
+
+              <div style={{ flex: 1 }}>
+                <Sidebar
+                  title="Profile"
+                  description="Projects worked on and experiences. If that matters. Just in case."
+                />
+                <Sidebar
+                  title="About author"
+                  description="Mukarram is a Full-stack Developer who loves to explore random tech shits. Loving RoRoR. Never heard? React on Ruby on Rails. Made few projects on Android as well. Based in Bengaluru."
+                />
+              </div>
+            </div>
+          )
+        }
+      </Media>
+    </div>
+  </div>
+);
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func
+};
+
+export default TemplateWrapper;
---
+++ src/pages/posts/15-13-2018-lazy-sunday.md
@@ -0,0 +1,9 @@
+---
+path: "/lazy-sunday"
+date: "2017-07-12T17:12:33.962Z"
+title: "My First Post"
+---
+
+Just trying how it works!
+
+This is really random, you might end up wasting your time reading this. I was trying to put blog on github pages and got to know about gatsby. Kinda interesting, will compe up with the tutorial soon.
